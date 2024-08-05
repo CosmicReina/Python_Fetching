8b919fe38daebea775ff48a2859e58a16f179807
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-06 02:44:31 +0900

    Update Main.py

--- src/BA_CHARACTER/Main.py
+++ src/BA_CHARACTER/Main.py
@@ -12,12 +12,43 @@ async def get_beautiful_soup(url: str) -> BeautifulSoup:
 
 
 async def download_images(hrefs: list[str]):
-    tasks = [download_image(href) for href in hrefs]
-    await asyncio.gather(*tasks)
+    async with aiohttp.ClientSession() as session:
+        tasks = [download_image(session, href) for href in hrefs]
+        await asyncio.gather(*tasks)
 
 
-async def download_image(url: str):
-    pass
+async def download_image(session: aiohttp.ClientSession, url: str):
+    beautiful_soup = await get_beautiful_soup(url)
+    portable_infobox = beautiful_soup.find("aside", {"class": "portable-infobox"})
+
+    # Name
+    h2 = portable_infobox.find("h2")
+    name_full = h2.text
+    name = name_full.split("/")[0].strip()
+
+    # Image
+    pi_image_collection = portable_infobox.find("div", {"class": "pi-image-collection"})
+    wds_tab__contents = pi_image_collection.find_all("div", {"class": "wds-tab__content"})[0:-1]
+
+    # Icon
+    icon_contents = wds_tab__contents[0]
+    icon_a = icon_contents.find("a")
+    icon_href = icon_a["href"]
+    await download_image_with_session(session, icon_href, f"icon/{name}.png")
+
+    # Portrait
+    portrait_contents = wds_tab__contents[1]
+    portrait_a = portrait_contents.find("a")
+    portrait_href = portrait_a["href"]
+    await download_image_with_session(session, portrait_href, f"portrait/{name}.png")
+
+
+async def download_image_with_session(session: aiohttp.ClientSession, url: str, file_name: str):
+    async with session.get(url) as response:
+        if response.status != 200:
+            raise Exception(f"Failed to fetch image: {response.status}")
+        with open(file_name, "wb") as file:
+            file.write(await response.read())
 
 
 if __name__ == "__main__":
@@ -33,4 +64,6 @@ if __name__ == "__main__":
         td = tr.find_all("td")[0]
         a = td.find("a")
         href = a["href"]
-        hrefs.append(href)
+        hrefs.append(url + href)
+
+    asyncio.run(download_images(["https://bluearchive.fandom.com/wiki/Aikiyo_Fuuka_(New_Year_ver.)"]))
