d6e01f70b32e7224db55f0f818a591affec6c0bb
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-07 19:58:57 +0900

    Update Main.py

--- src/BA_CHARACTER/Main.py
+++ src/BA_CHARACTER/Main.py
@@ -19,123 +19,62 @@ async def download_images(hrefs: list[str]):
 
 
 async def download_image(session: aiohttp.ClientSession, url: str):
-    try:
-        print(f"Download: {url}...")
-        start = time.time()
+    print(f"Download: {url}...")
+    start = time.time()
 
-        beautiful_soup = await get_beautiful_soup(url)
-        infobox = beautiful_soup.find("aside", {"class": "portable-infobox"})
-        # print(infobox)
+    beautiful_soup = await get_beautiful_soup(url)
+    infobox = beautiful_soup.find("aside", {"class": "portable-infobox"})
 
-        # Name
-        h2 = infobox.find("h2")
-        name = h2.text.split("/")[0].strip()
+    # Name
+    h2 = infobox.find("h2")
+    name = h2.text.split("/")[0].strip()
 
-        tabber = infobox.find(class_=["wds-tabber"])
-        print(tabber)
+    # Content
+    tabber = infobox.find(class_=["wds-tabber"])
+    contents = tabber.find_all("div", class_="wds-tab__content", recursive=False)
 
-        contents = tabber.find_all("div", class_="wds-tab__content", recursive=False)
-        # print("\n\n".join([str(wds_tab__content) for wds_tab__content in contents]))
-        # print(contents)
-
-        # Icon
-        content_icon = contents[0]
-        # print(content_icon)
-        content_icon_contents = content_icon.find_all("div", {"class": "wds-tab__content"})
-        if content_icon_contents:
-            for content in content_icon_contents:
-                a = content.find("a")
-                icon_href = a["href"]
-                print(icon_href)
-        else:
-            a = content_icon.find("a")
+    # Icon
+    content_icon = contents[0]
+    content_icon_contents = content_icon.find_all("div", {"class": "wds-tab__content"})
+    if content_icon_contents:
+        for content in content_icon_contents:
+            a = content.find("a")
+            icon_title = a["title"]
             icon_href = a["href"]
-            print(icon_href)
+            file_name = "icon/" + name + "_" + icon_title
+            await download_with_session(session, icon_href, file_name, "png")
+    else:
+        a = content_icon.find("a")
+        icon_href = a["href"]
+        file_name = "icon/" + name
+        await download_with_session(session, icon_href, file_name, "png")
 
-        # Portrait
-        content_portrait = contents[1]
-        # print(content_portrait)
-        content_portrait_contents = content_portrait.find_all("div", {"class": "wds-tab__content"})
-        if content_portrait_contents:
-            for content in content_portrait_contents:
-                a = content.find("a")
-                portrait_href = a["href"]
-                print(portrait_href)
-        else:
-            a = content_portrait.find("a")
+    # Portrait
+    content_portrait = contents[1]
+    content_portrait_contents = content_portrait.find_all("div", {"class": "wds-tab__content"})
+    if content_portrait_contents:
+        for content in content_portrait_contents:
+            # URL
+            a = content.find("a")
+            portrait_title = a["title"]
             portrait_href = a["href"]
-            print(portrait_href)
+            file_name = "portrait/" + name + "_" + portrait_title
+            await download_with_session(session, portrait_href, file_name, "png")
+    else:
+        a = content_portrait.find("a")
+        portrait_href = a["href"]
+        file_name = "portrait/" + name
+        await download_with_session(session, portrait_href, file_name, "png")
+
+    end = time.time()
+    print(f"Download: {url} finished in {end - start:.2f}s")
 
 
-        # print("1.")
-        # # print(contents[0])
-        #
-        # print("2.")
-        # # print(contents[1])
-        #
-        # wds_tab__contents_contents = contents[1].find_all("div", {"class": "wds-tab__content"})
-        # print("\n\n".join([str(wds_tab__content) for wds_tab__content in wds_tab__contents_contents]))
-
-
-        # Contents
-        # tabber = infobox.find_all("div", {"class": "wds-tabber"})[0]
-        # print(tabber)
-        # contents = tabber.find_all("div", {"class": "wds-tab__content"})
-        #
-        # # Icon
-        # wds_tab__content_icon = contents[0]
-        # a_icon = wds_tab__content_icon.find("a")
-        # icon_href = a_icon["href"]
-        # print(icon_href)
-        #
-        # # Portrait
-        # wds_tab__content_portrait = contents[1]
-        # print(wds_tab__content_portrait)
-
-        # # Portrait
-        # wds_tab__content_portrait = contents[1]
-        # print(wds_tab__content_portrait)
-        # pi_image_collection = wds_tab__content_portrait.find("div", {"class": "pi-image-collection"})
-        # if pi_image_collection:
-        #     print("Case 1")
-        #     wds_tab__content_portraits = pi_image_collection.find_all("div", {"class": "wds-tab__content"})
-        #     print(len(wds_tab__content_portraits))
-        #     for wds_tab__content_portrait in wds_tab__content_portraits:
-        #         a_portrait = wds_tab__content_portrait.find("a")
-        #         portrait_href = a_portrait["href"]
-        #         print(portrait_href)
-        # else:
-        #     print("Case 2")
-        #     a_portrait = wds_tab__content_portrait.find("a")
-        #     portrait_href = a_portrait["href"]
-        #     print(portrait_href)
-
-        # contents = infobox.find_all("div", {"class": "wds-tab__content"})
-        #
-        # wds_tab__content_icon = contents[0]
-        # print(wds_tab__content_icon)
-        # a_icon = wds_tab__content_icon.find("a")
-        # icon_href = a_icon["href"]
-        # await download_image_with_session(session, icon_href, f"icon/{name}.png")
-        #
-        # print("\n\n")
-        #
-        # wds_tab__content_portrait = contents[1]
-        # print(wds_tab__content_portrait)
-        # a_portrait = wds_tab__content_portrait.find("a")
-        # portrait_href = a_portrait["href"]
-        # await download_image_with_session(session, portrait_href, f"portrait/{name}.png")
-
-        end = time.time()
-        print(f"Download: {url} finished in {end - start:.2f}s")
-    except Exception as e:
-        print(f"Failed to download: {url} - {e}")
-
-
-async def download_image_with_session(session: aiohttp.ClientSession, url: str, file_name: str):
+async def download_with_session(session: aiohttp.ClientSession, url: str, name: str, type: str):
+    file_name = f"{name}.{type}"
     async with session.get(url) as response:
         if response.status != 200:
-            raise Exception(f"Failed to fetch image: {response.status}")
+            raise Exception(f"Failed to fetch image: {file_name} - {response.status}")
         with open(file_name, "wb") as file:
             file.write(await response.read())
 
@@ -155,7 +94,8 @@ if __name__ == "__main__":
         href = a["href"]
         hrefs.append(url + href)
 
-    # asyncio.run(download_images(hrefs))
-    asyncio.run(download_images(["https://bluearchive.fandom.com/wiki/Aikiyo_Fuuka_(New_Year_ver.)"]))
+    asyncio.run(download_images(hrefs))
+    # asyncio.run(download_images(["https://bluearchive.fandom.com/wiki/Aikiyo_Fuuka_(New_Year_ver.)"]))
     # asyncio.run(download_images(["https://bluearchive.fandom.com/wiki/Wanibuchi_Akari"]))
+    # asyncio.run(download_images(["https://bluearchive.fandom.com/wiki/Murokasa_Akane"]))
     # asyncio.run(download_images(["https://bluearchive.fandom.com/wiki/Hayase_Yuuka"]))
