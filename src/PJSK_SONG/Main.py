8b6d88a3e3f3a4469a3137c3ec1abb5789039c70
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-12 22:59:37 +0900

    Update

--- src/PJSK_SONG/Main.py
+++ src/PJSK_SONG/Main.py
@@ -12,6 +12,9 @@ type_cover_songs = "cover_songs"
 type_commissioned_songs = "commissioned_songs"
 type_contest_songs = "contest_songs"
 
+url_fandom = "https://projectsekai.fandom.com"
+url_song_list = "https://projectsekai.fandom.com/wiki/Song_List"
+
 
 async def get_beautiful_soup(url: str) -> BeautifulSoup:
     async with aiohttp.ClientSession() as session:
@@ -21,19 +24,39 @@ async def get_beautiful_soup(url: str) -> BeautifulSoup:
             return BeautifulSoup(await response.text(), "html.parser")
 
 
-async def fetch_song(url: str, song_type: str):
-    beautiful_soup = asyncio.run(get_beautiful_soup(url))
+async def fetch_songs(list_songs: list):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_song(session, song["url"], song["type_song"]) for song in list_songs]
+        await asyncio.gather(*tasks)
+
+
+async def fetch_song(session: aiohttp.ClientSession, url: str, type_song: str):
+    beautiful_soup = await get_beautiful_soup(url)
     article_table = beautiful_soup.find_all("table", class_="article-table")
+
+    song_title = beautiful_soup.find("h2", class_="pi-title").text
+    if song_title is None:
+        return
+
     trs_song = []
     for table in article_table:
         trs = table.find_all("tr")
         for tr in trs[1:]:
             trs_song.append(tr)
 
+    file_directory = f"songs/{type_song}/{song_title}"
+    if len(trs_song) != 0:
+        os.mkdir(file_directory)
+
     for tr in trs_song:
-        no = tr.find_all("td")[0].text
-        title = tr.find_all("td")[1].text
+        no = tr.find_all("td")[0].text.strip()
+        title = tr.find_all("td")[1].text.strip()
         song = tr.find_all("td")[-1].find("audio")
+        src = song["src"]
+
+        name = f"{no} - {title}"
+        if song is not None:
+            await download_with_session(session, src, f"{file_directory}/{name}", "mp3")
 
 
 async def download_with_session(session: aiohttp.ClientSession, url: str, name: str, type: str):
@@ -67,40 +90,37 @@ def setup():
 
 
 def get_list_songs_of_type(wikitable: BeautifulSoup, type_song: str) -> list:
-    url_fandom = "https://projectsekai.fandom.com"
-    list_songs = []
-    trs = wikitable.find_all("tr")[1:]
-    for tr in trs:
-        td = tr.find_all("td")[0]
-        a = td.find("a")
-        url = url_fandom + a["href"]
-        list_songs.append({
-            "type_song": type_song,
-            "url": url
-        })
-    return list_songs
+    return [{
+        "type_song": type_song,
+        "url": url_fandom + tr.find_all("td")[0].find("a")["href"]
+    } for tr in wikitable.find_all("tr")[1:]]
 
 
 def main():
     setup()
 
-    url_song_list = "https://projectsekai.fandom.com/wiki/Song_List"
     beautiful_soup = asyncio.run(get_beautiful_soup(url_song_list))
     wikitables = beautiful_soup.find_all("table", class_="wikitable")
 
-    pre_existing_songs = wikitables[0]
-    cover_songs = wikitables[1]
-    commissioned_songs = wikitables[2]
-    contest_songs = wikitables[3]
+    # pre_existing_songs = wikitables[0]
+    # cover_songs = wikitables[1]
+    # commissioned_songs = wikitables[2]
+    # contest_songs = wikitables[3]
+    #
+    # list_pre_existing_songs = get_list_songs_of_type(pre_existing_songs, type_pre_existing_songs)
+    # list_cover_songs = get_list_songs_of_type(cover_songs, type_cover_songs)
+    # list_commissioned_songs = get_list_songs_of_type(commissioned_songs, type_commissioned_songs)
+    # list_contest_songs = get_list_songs_of_type(contest_songs, type_contest_songs)
+    #
+    # list_total_songs = list_pre_existing_songs + list_cover_songs + list_commissioned_songs + list_contest_songs
+    # songs_amount = len(list_total_songs)
+    # print(f"Fetching {songs_amount} songs...")
 
-    list_pre_existing_songs = get_list_songs_of_type(pre_existing_songs, type_pre_existing_songs)
-    list_cover_songs = get_list_songs_of_type(cover_songs, type_cover_songs)
-    list_commissioned_songs = get_list_songs_of_type(commissioned_songs, type_commissioned_songs)
-    list_contest_songs = get_list_songs_of_type(contest_songs, type_contest_songs)
-
-    list_total_songs = list_pre_existing_songs + list_cover_songs + list_commissioned_songs + list_contest_songs
-
-    print("\n".join([str(song) for song in list_total_songs]))
+    # Test
+    asyncio.run(fetch_songs([{
+        "type_song": type_commissioned_songs,
+        "url": "https://projectsekai.fandom.com/wiki/Kitty"
+    }]))
 
 
 if __name__ == "__main__":
