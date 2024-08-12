c3340ec17503b0d0eefe2d026628b0d459f2f7bc
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-12 21:55:18 +0900

    Update Main.py

--- src/PJSK_SONG/Main.py
+++ src/PJSK_SONG/Main.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+import shutil
 
 import aiohttp
 from bs4 import BeautifulSoup
@@ -12,15 +14,36 @@ async def get_beautiful_soup(url: str) -> BeautifulSoup:
             return BeautifulSoup(await response.text(), "html.parser")
 
 
+def setup():
+    print("Setting up...")
+
+    if os.path.exists("songs"):
+        print("Removing existing 'songs' directory...")
+        shutil.rmtree("songs")
+
+    print("Creating 'songs' directory...")
+    os.mkdir("songs")
+    print("Creating 'songs/pre-existing_songs' directory...")
+    os.mkdir("songs/pre-existing_songs")
+    print("Creating 'songs/cover_songs' directory...")
+    os.mkdir("songs/cover_songs")
+    print("Creating 'songs/commissioned_songs' directory...")
+    os.mkdir("songs/commissioned_songs")
+    print("Creating 'songs/contest_songs' directory...")
+    os.mkdir("songs/contest_songs")
+
+    print("Setup complete!\n")
+
+
 def main():
+    setup()
+
     url = "https://projectsekai.fandom.com/wiki/Song_List"
     beautiful_soup = asyncio.run(get_beautiful_soup(url))
     wikitables = beautiful_soup.find_all("table", class_="wikitable")
     pre_existing_songs = wikitables[0]
     cover_songs = wikitables[1]
     commissioned_songs = wikitables[2]
-    trs = commissioned_songs.find_all("tr")
-    print(len(trs))
     contest_songs = wikitables[3]
 
 
