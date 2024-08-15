585f81bf140eb6e32b0c27e8da99d8c6d3040237
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-15 21:43:01 +0900

    Update Main_New.py

--- src/PJSK_SONG/Main_New.py
+++ src/PJSK_SONG/Main_New.py
@@ -67,7 +67,13 @@ def setup():
 
 
 def fetch():
-    pass
+    beautiful_soup = get_beautiful_soup_requests(url_song_list)
+    wikitables = beautiful_soup.find_all("table", class_="wikitable")
+
+    pre_existing_songs = wikitables[0]
+    cover_songs = wikitables[1]
+    commissioned_songs = wikitables[2]
+    contest_songs = wikitables[3]
 
 
 def main():
