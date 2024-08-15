29476376ff9a314ddc8f1c85b37cc1961986692f
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-15 22:00:05 +0900

    Update Main_New.py

--- src/PJSK_SONG/Main_New.py
+++ src/PJSK_SONG/Main_New.py
@@ -45,6 +45,13 @@ def get_beautiful_soup_requests(url: str) -> BeautifulSoup:
     return BeautifulSoup(response.text, 'html.parser')
 
 
+def get_list_song_of_type(wikitable: BeautifulSoup, type: str) -> list:
+    return [{
+        "type_song": type,
+        "url": url_fandom + tr.find_all("td")[0].find("a")["href"]
+    } for tr in wikitable.find_all("tr")[1:]]
+
+
 def setup():
     print("Setting up...")
 
@@ -74,6 +81,14 @@ def fetch():
     cover_songs = wikitables[1]
     commissioned_songs = wikitables[2]
     contest_songs = wikitables[3]
+
+    list_pre_existing_songs = get_list_song_of_type(pre_existing_songs, type_pre_existing_songs)
+    list_cover_songs = get_list_song_of_type(cover_songs, type_cover_songs)
+    list_commissioned_songs = get_list_song_of_type(commissioned_songs, type_commissioned_songs)
+    list_contest_songs = get_list_song_of_type(contest_songs, type_contest_songs)
+
+    list_total_songs = list_pre_existing_songs + list_cover_songs + list_commissioned_songs + list_contest_songs
+    print("\n".join([str(song) for song in list_total_songs]))
 
 
 def main():
